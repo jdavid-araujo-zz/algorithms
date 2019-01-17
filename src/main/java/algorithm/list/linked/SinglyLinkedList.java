74841783b03b663bf48915a8173cfd7258bb99e6
jdavid-araujo <j.davidsousaaraujo@gmail.com>
2019-01-17 12:42:54 -0300

    Add Tail to SinglyLinkedList

---
+++ src/main/java/algorithm/list/linked/SinglyLinkedList.java
@@ -0,0 +1,60 @@
+package algorithm.list.linked;
+
+public class SinglyLinkedList<T> {
+
+	private Node<T> head, tail;
+	private int size;
+	
+	public void insert(T data) {		
+		this.addLast(data); // or addFirst
+	}
+	
+	public void addFirst(T data) {
+		Node<T> newNode = new Node<T>(data);
+		newNode.setNextNode(this.head);
+		
+		if(this.isEmpty()) {
+			this.head = this.tail;
+		}
+		
+		this.head = newNode;		
+		++this.size;
+	}
+	
+	public void addLast(T data) {
+		Node<T> newNode = new Node<T>(data);
+		newNode.setNextNode(null);
+		
+		if(this.isEmpty()) {
+			this.head = newNode;
+		} else {
+			this.tail.setNextNode(newNode);
+		}
+		
+		this.tail = newNode;
+		++this.size;
+
+	}
+	
+	public void tranverseList() {
+		
+		if(this.head == null) {
+			return;
+		}
+		
+		Node<T> node = this.head;
+		
+		while(node != null) {		
+			
+			System.out.print(node.getData() + " -> ");
+			
+			node = node.getNextNode();
+		}
+	}
+	
+	public boolean isEmpty() {
+		return (this.size == 0);
+	}
+
+
+}
