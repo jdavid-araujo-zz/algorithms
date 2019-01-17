9d7bcdeecb41d53398e15efcefb32d0263973c0e
jdavid-araujo <j.davidsousaaraujo@gmail.com>
2019-01-17 12:50:24 -0300

    Add removeFirst to SinglyLinkedList

--- src/main/java/algorithm/list/linked/SinglyLinkedList.java
+++ src/main/java/algorithm/list/linked/SinglyLinkedList.java
@@ -4,57 +4,73 @@ public class SinglyLinkedList<T> {
 
 	private Node<T> head, tail;
 	private int size;
-	
-	public void insert(T data) {		
+
+	public void insert(T data) {
 		this.addLast(data); // or addFirst
 	}
-	
+
 	public void addFirst(T data) {
 		Node<T> newNode = new Node<T>(data);
 		newNode.setNextNode(this.head);
-		
-		if(this.isEmpty()) {
+
+		if (this.isEmpty()) {
 			this.head = this.tail;
 		}
-		
-		this.head = newNode;		
+
+		this.head = newNode;
 		++this.size;
 	}
-	
+
 	public void addLast(T data) {
 		Node<T> newNode = new Node<T>(data);
 		newNode.setNextNode(null);
-		
-		if(this.isEmpty()) {
+
+		if (this.isEmpty()) {
 			this.head = newNode;
 		} else {
 			this.tail.setNextNode(newNode);
 		}
-		
+
 		this.tail = newNode;
 		++this.size;
 
 	}
-	
+
 	public void tranverseList() {
-		
-		if(this.head == null) {
+
+		if (this.head == null) {
 			return;
 		}
-		
+
 		Node<T> node = this.head;
-		
-		while(node != null) {		
-			
+
+		while (node != null) {
 			System.out.print(node.getData() + " -> ");
-			
 			node = node.getNextNode();
 		}
 	}
-	
+
 	public boolean isEmpty() {
 		return (this.size == 0);
 	}
 
+	public T removeFirst() {
+
+		if (this.isEmpty()) {
+			return null;
+		}
+
+		T data = this.head.getData();
+
+		this.head = this.head.getNextNode();
+
+		--this.size;
+
+		if (this.isEmpty()) {
+			this.tail = null;
+		}
+
+		return data;
+	}
 
 }
